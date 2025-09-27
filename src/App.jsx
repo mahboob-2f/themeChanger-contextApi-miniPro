9b5691ef26bf33f1d168edc97f35c4f2ecd8dc57
mahboob <mahboob6550@gmail.com>
2025-09-27 23:45:17 +0530

    tailwind setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ 
 
 function App() {
   
 
   return (
     <>
+      <p className="text-2xl text-center font-serif  p-4 text-[green] font-bold">hello ghost</p> 
        
     </>
   )
